6af0f19395439c8c5c2f3157984a93b8e756da49
John.Spencer.Terry <John.Spencer.Terry@gmail.com>
2021-02-22 22:18:19 -0500

    changes

---
+++ server/index.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// http://localhost:3001/api
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
